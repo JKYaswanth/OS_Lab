6c8eb7a2a467ddf6b4fb750072891d4284761d99
JK Yaswanth <ampilijayakumar932@gmail.com>
2023-08-01 23:54:00 +0530

    partial_code

---
+++ worst_fit.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    
+    int bsize[20],psize[20];
+    int all[20],m,n,i,j;
+    printf("Enter the number of blocks :- ");
+    scanf("%d",&m);
+    printf("Enter the size of each block :- ");
+    for(i=0;i<m;i++){
+        scanf("%d",&bsize[i]);
+    }
+    
+    printf("Enter the number of processes :- ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        
+    }
+    return 0;
+}
